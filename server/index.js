82302f203fd1acf263df539e20d624b2f7f4ce3a
harsh <h.munshi@oxolo.com>
2022-05-19 17:11:10 +0200

    [new][fix] swagger documentation, API to generate mnemonic

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require("cors");
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require("swagger-ui-express");
+const { mnemonicGenerate, cryptoWaitReady } = require('@polkadot/util-crypto');
+const { keyring } = require('@polkadot/ui-keyring');
+const assert = require('assert');
 
 // Defining a port
 const PORT = process.env.PORT || 3001;
@@ -36,19 +39,77 @@ app.get("/", (req, res) => {
     res.json({message: "Hey hi! This is the backend. You can use other APIs and build on top of it."})
 });
 
-/** 
- * create a default test endpoint
- * @param {req} request body from the front end
- * @param {req.query.password} string encrypted password
- * @param {req.query.user} string user name
+/**
+ * @swagger 
+ *  /test_params:
+ *  post:
+ *      description: create a default test endpoint
+ *      parameters:
+ *          - in: query
+ *            name: user
+ *            schema:
+ *              type: string
+ *          - in: query
+ *            name: password
+ *            schema:
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: Success
+ *          404:
+ *              description: ID or Password not found
  */
  app.post("/test_params", (req, res) => {
+     // extract ID and password
     const password = req.query.password;
     const username = req.query.user;
-    res.json({message: "Welcome "+username});
+
+    // check for mandatory tests
+    try {
+        assert(typeof(password) !== undefined);
+        assert(typeof(username) !== undefined);
+        assert(password.length > 7);
+        res.json({message: "Welcome "+username});
+    } catch {
+        res.status(404).send('Check if ID or Password is not empty');
+    }
+    
 });
 
 
-app.listen(PORT, () => {
+/**
+ * @swagger 
+ *  /generate_mnemonic:
+ *  post:
+ *      description: create an endpoint to generate the mnemonic
+ *      parameters:
+ *          - in: query
+ *            name: words
+ *            schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Success
+ *          404:
+ *              description: unsupported word length 
+ */
+app.post("/generate_mnemonic", (req, res) => {
+    // generate the mnemonic
+    const nWords = parseInt(req.query.words);
+    try {
+        assert(nWords !== 0);
+        assert(nWords === 12 || nWords === 24);
+        const mnemonic = mnemonicGenerate(nWords);
+        res.json({mnemonic: mnemonic});
+    } catch {
+        res.status(404).send('Length of the words can be either 12 or 24');
+    }
+});
+
+
+cryptoWaitReady().then(() => app.listen(PORT, () => {
+    // initialize it with ss58Format 42 for default substrate format (for azero)
+    keyring.loadAll({ ss58Format: 42, type: 'sr25519' });  
     console.log('Server listening to a port')
-    });
+   })
+);
