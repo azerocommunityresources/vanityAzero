e1d35f3589ecf2ec5bd558c5ac83d11e4d8fe73c
harsh <h.munshi@oxolo.com>
2022-05-20 21:25:51 +0200

    [new] functional API call to generate address / keystore json

--- server/index.js
+++ server/index.js
@@ -107,6 +107,55 @@ app.post("/generate_mnemonic", (req, res) => {
 });
 
 
+/**
+ * @swagger 
+ *  /generate_address:
+ *  post:
+ *      description: create a wallet address using the mnemonic, ID and password
+ *      parameters:
+ *          - in: query
+ *            name: accountName
+ *            schema:
+ *              type: string
+ *          - in: query
+ *            name: password
+ *            schema:
+ *              type: string
+ *          - in: query
+ *            name: mnemonic
+ *            schema:
+ *              type: json
+ *      responses:
+ *          200:
+ *              description: Success
+ *          404:
+ *              description: none of the fields should be empty 
+ */
+app.post("/generate_address", (req, res) => {
+    // check for account ID, password and mnemonic
+    const accoutName = req.query.accountName;
+    const password = req.query.password;
+
+    // mnemonic is parsed as a json from the /generate_mnemonic endpoint
+    const mnemonic = req.query.mnemonic;
+
+    // sample testing
+    console.log(mnemonic, accoutName, password);
+    try {
+        assert(typeof(accoutName) !== undefined);
+        assert(typeof(password) !== undefined); 
+        assert(typeof(mnemonic) !== undefined);
+
+
+        // add the account, encrypt the stored JSON with an account-specific password
+        const { pair, json } = keyring.addUri(mnemonic, password, { name: accoutName });
+        res.json({keystore: json});
+    } catch {
+        res.status(404).send('check if the input are not empty');
+    }
+});
+
+
 cryptoWaitReady().then(() => app.listen(PORT, () => {
     // initialize it with ss58Format 42 for default substrate format (for azero)
     keyring.loadAll({ ss58Format: 42, type: 'sr25519' });  
