295ad60009502e48a7d263186dc49368d7fe1802
harsh <h.munshi@oxolo.com>
2022-05-25 15:35:44 +0200

    [fix][new] functional Rust example

--- examples/rust/connect/src/main.rs
+++ examples/rust/connect/src/main.rs
@@ -1,14 +1,27 @@
 use reqwest;
 use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Post {
+    words: i32 
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    mnemonic: String
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp: serde_json::Value = reqwest::Client::new()
+async fn main() -> Result<(), reqwest::Error> {
+    let payload = Post {
+        words: 12
+    };
+
+    let resp: Response = reqwest::Client::new()
         .post("http://localhost:3001/generate_mnemonic")
-        .json(&serde_json::json!({
-            "words": 12,
-        }))
+        .json(&payload)
         .send()
         .await?
         .json()
